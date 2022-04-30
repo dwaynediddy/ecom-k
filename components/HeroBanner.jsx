4db483ee981035432952c32f1ecd3c62e5278898
dwaynediddy <burrows.dwayne@icloud.com>
2022-04-30 16:27:56 +0200

    added products to sanity and react components

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import {urlFor} from '../lib/client'
+
+const HeroBanner = ({ heroBanner }) => {
+  return (
+    <div className="hero-banner-container">
+        <div>
+            <p className='beats-solo'>{heroBanner.smallText}</p>
+            <h3>{heroBanner.midText}</h3>
+            <img src={urlFor(heroBanner.image)} alt='main' className='hero-banner-image' />
+            <div>
+                <Link href={`/product/${heroBanner.product}`}>
+                    <button type='button'>{heroBanner.buttonText}</button>
+                </Link>
+                <div className='desc'>
+                    <h5>{heroBanner.desc}</h5>
+                    <p>{heroBanner.smallText}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroBanner
