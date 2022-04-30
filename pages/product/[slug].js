40cfd2b08d6c911bc45de1bff92a0f48beb41ba1
dwaynediddy <burrows.dwayne@icloud.com>
2022-04-30 22:31:58 +0200

    added slugs layout page for products

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { client, urlFor } from '../../lib/client'
 
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
+
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product
 
@@ -10,6 +12,45 @@ const ProductDetails = ({ product, products }) => {
             <div>
                 <div className="image-container">
                     <img src={urlFor(image && image[0])}/>
+                </div>
+                {/* <div className="small-images-container">
+                    {image?.map((item, i) => (
+                        <img 
+                            src={urlFor(item )}
+                            className=''
+                            onMouseEnter=''
+                        />
+                    ))}
+                </div> */}
+            </div>
+            <div className="product-detail-desc">
+                <h1>{name}</h1>
+                <div className="reviews">
+                    <div>
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiOutlineStar />
+                    </div>
+                    <p>(20)</p>
+                </div>
+                <div>
+                    <h4>Details: </h4>
+                    <p>{details}</p>
+                    <p>${price}</p>
+                    <div className="quantity">
+                        <h3>Quantity</h3>
+                        <p className='quantity-desc'>
+                            <span className='minus' onClick=''><AiOutlineMinus /></span>
+                            <span className='num' onClick=''>0</span>
+                            <span className='plus' onClick=''><AiOutlinePlus /></span>
+                        </p>
+                    </div>
+                    <div className='buttons'>
+                        <button type='button' className='add-to-cart' onClick=''>Add to Cart</button>
+                        <button type='button' className='buy-now' onClick=''>Buy Now</button>
+                    </div>
                 </div>
             </div>
         </div>
