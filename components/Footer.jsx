1d652031767f04cf94dfcf780a1691eade1a2c4f
dwaynediddy <burrows.dwayne@icloud.com>
2022-05-03 18:15:03 +0200

    added linking to socials

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,19 @@
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai'
+
 const Footer = () => {
   return (
     <div className="footer-container">
         <p>© 2022 Kepac straya All Rights Reserved</p>
         <p className="icons">
-            <AiFillInstagram />
+          <a target='_blank' href='https://www.instagram.com/kepac.coffee/' rel="noopener noreferrer">
+            <AiFillInstagram  />
+          </a>
+          <a target='_blank' href='https://www.instagram.com/kepac.coffee/' rel="noopener noreferrer">
             <AiOutlineTwitter />
+          </a>
+          <a target='_blank' href='https://www.instagram.com/kepac.coffee/' rel="noopener noreferrer">
+            <AiFillFacebook />
+          </a>
         </p>
     </div>
   )
